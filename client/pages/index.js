59eb63356e50e141b3ad8153bc39e60fcb7c554c
Kyano DeBeuckelaere <kyano.debeuckelaere12@gmail.com>
2024-06-28 11:13:29 +0200

    added netlify

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 import io from "socket.io-client";
 
 
-const socket = io(process.env.NEXT_PUBLIC_SOCKETIO, {
+const socket = io(process.env.NEXT_PUBLIC_SOCKETIO, { path: "/socket.io" }, {
     reconnection: true,
 })
 
--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ dotenv.config();
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
+    path: "/socket.io",
     cors: {
         origin: [process.env.CLIENT_URL_MAIN, process.env.CLIENT_URL_SECONDARY],
         methods: ["GET", "POST"],
